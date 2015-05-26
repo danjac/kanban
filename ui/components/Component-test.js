1acece7a5df38f2e04ece94a7c90ff00134fbb21
danjac <danjac354@gmail.com>
2015-05-26 12:02:34 +0300

    removed test dir

--- ui/components/Component-test.js
+++ ui/components/Component-test.js
@@ -2,11 +2,14 @@ import React from 'react';
 import TestUtils from 'react/lib/ReactTestUtils'; //I like using the Test Utils, but you can just use the DOM API instead.
 import expect from 'expect';
 
-import Container from '../../ui/components/Container';
+import AppFlux from '../flux';
+import Container from './Container';
+
 
 describe('root', function () {
   it('renders the root container!', function () {
-    const root = TestUtils.renderIntoDocument(<Container />);
+    const flux = new AppFlux();
+    const root = TestUtils.renderIntoDocument(<Container flux={flux}/>);
     expect(root).toExist();
   });
 
