b91189281f19df8235c5bdc5b366dab590d27202
danjac <danjac354@gmail.com>
2015-05-27 14:40:48 +0300

    test changes

--- test/components/Container.js
+++ test/components/Container.js
@@ -4,23 +4,20 @@ import TestUtils from 'react/lib/ReactTestUtils';
 import expect from 'expect';
 
 import AppFlux from '../../ui/flux';
+import api from '../../ui/api';
 import Container from '../../ui/components/Container';
 
 describe('root', function () {
 
     it('renders the root container!', function () {
-
-
         const flux = new AppFlux();
-
-        const actions = flux.getActions("taskLists");
-
-        sinon.stub(actions, "getBoard");
-
+        sinon.stub(api, "getBoard").returns({ lists: [
+            {
+            name: "test",
+            tasks: []
+            }] });
         const root = TestUtils.renderIntoDocument(<Container flux={flux}/>);
-        const data = {
-            lists: []
-        };
+
         expect(root).toExist();
     });
 
